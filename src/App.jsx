66521d623ee04a3d71aedc914a9d58e33d8c7c3b
mahaveer <mahaveermnadloi9@gmail.com>
2024-02-29 18:06:28 +0530

    app changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ const App = () => {
         <div className="text-red-700 text-6xl  p-20">App</div>
 
         <p className="text-5xl p-10 ">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto,
-          id neque. Ullam facilis architecto voluptates dignissimos deleniti
-          voluptas ab amet, quos necessitatibus placeat temporibus, rerum
-          nesciunt id veritatis. Minima, iure!
+  
         </p>
 
         
